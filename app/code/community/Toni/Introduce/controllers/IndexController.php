c1f91d06cfd3590d910c09707e3dc41d0e2f440e
lolerpnv <lolerpnv@gmail.com>
2016-09-06 14:06:29 +0200

    Chapter 2

--- app/code/community/Toni/Introduce/controllers/IndexController.php
+++ app/code/community/Toni/Introduce/controllers/IndexController.php
@@ -4,18 +4,13 @@ class Toni_Introduce_IndexController extends Mage_Core_Controller_Front_Action
     public function indexAction() {
         $this->loadLayout();
 
-        $block = $this->getLayout()->createBlock('introduce/template');
+        //$block = $this->getLayout()->createBlock('introduce/template');
 
-        //$block->setText("Textual text");
-
-
-
-        $this->getLayout()->getBlock('content')->insert($block);
-
-
+        //$this->getLayout()->getBlock('content')->insert($block);
 
         $this->renderLayout();
     }
+
     public function testUserSaveAction() {
 
         $user = Mage::getModel('introduce/user');
---
+++ app/code/community/Toni/MaxOrderAmount/Helper/Data.php
@@ -0,0 +1,63 @@
+<?php
+
+class Toni_MaxOrderAmount_Helper_Data extends Mage_Core_Helper_Data
+{
+
+    const XML_PATH_ACTIVE =
+
+        'sales/toni_maxorderamount/active';
+
+    const XML_PATH_SINGLE_ORDER_TOP_AMOUNT =
+
+        'sales/toni_maxorderamount/single_order_top_amount';
+
+    const XML_PATH_SINGLE_ORDER_TOP_AMOUNT_MSG =
+
+        'sales/toni_maxorderamount/single_order_top_amount_msg';
+
+
+    public function isModuleEnabled($moduleName = null)
+
+    {
+
+        if ((int)Mage::getStoreConfig(self::XML_PATH_ACTIVE,
+
+                Mage::app()->getStore()) != 1) {
+
+            return false;
+
+        }
+
+        return parent::isModuleEnabled($moduleName );
+
+    }
+
+    public function getSingleOrderTopAmount($store = null)
+
+    {
+
+        return
+
+            (int)Mage::getStoreConfig
+
+            (self::XML_PATH_SINGLE_ORDER_TOP_AMOUNT, $store);
+
+    }
+
+
+
+    public function getSingleOrderTopAmountMsg($store = null)
+
+    {
+
+        return
+
+            Mage::getStoreConfig
+
+            (self::XML_PATH_SINGLE_ORDER_TOP_AMOUNT_MSG,
+
+                $store);
+
+    }
+
+}
